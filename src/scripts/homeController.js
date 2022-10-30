94ca339fcce3279aa6f5ead8e283fa6fb5a6ef01
Petromir Kolev <petromirkolev@gmail.com>
2022-10-30 08:28:22 +0200

    Integrate calendar in app

--- src/scripts/homeController.js
+++ src/scripts/homeController.js
@@ -1,1 +1,18 @@
+// Import stylesheet
 import "../css/home.css";
+
+// Import calendar  module
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+
+let calendar = new Calendar(calendarEl, {
+  plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+  initialView: "dayGridMonth",
+  headerToolbar: {
+    left: "prev,next today",
+    center: "title",
+    right: "dayGridMonth,timeGridWeek,listWeek",
+  },
+});
